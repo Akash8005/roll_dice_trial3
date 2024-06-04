ea3a29946b7a9cd3bdac1947982fe762f4070cd2
akash yanakandla <akashyanakandla0073@gmail.com>
2024-06-04 23:44:25 +0530

    this is the second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Jai Shree Ram"),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Colors.deepOrange,
+              Colors.orange,
+            ],
+          )),
+          child: const Center(
+            child: Text(
+              "Jai Shree Ram",
+              style: TextStyle(fontSize: 100, color: Colors.white),
+            ),
+          ),
         ),
       ),
     ),
